303ff19592b2ec3deae98bee610a0270a8ffc01e
20KD1A0598 <113771486+20KD1A0598@users.noreply.github.com>
2022-09-17 16:37:09 +0530

    Add files via upload

---
+++ haskellpr.hs
@@ -0,0 +1,7 @@
+--Write a function f1 :: [a] -> Int -> [a] that takes a list and a
+--number n, and drops elements of the list at index i*n-1, for i > 0.
+--Remember that list indices start at 0.
+f1 :: [a] -> Int -> [a]
+f1 xs n = undefined
+
+main :: IO ()
